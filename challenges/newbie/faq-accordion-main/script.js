34b8f8370ca656d10729293b3d3c5c362abec968
liandracruz <liandraluiza.02@gmail.com>
2026-03-31 22:17:18 -0300

    verifying other answers function

--- challenges/newbie/faq-accordion-main/script.js
+++ challenges/newbie/faq-accordion-main/script.js
@@ -6,6 +6,21 @@ questions.forEach(question => {
         const answer = question.nextElementSibling;
         const icon = question.querySelector('img');
 
+        questions.forEach(item => {
+            if(item !== question) {
+                const otherAnswer = item.nextElementSibling;
+                otherAnswer.classList.remove('active');
+
+                const otherIcon = item.querySelector('img');
+                otherIcon.src = 'assets/images/icon-plus.svg';
+            }
+
+            const otherAnswer = item.nextElementSibling;
+            const otherIcon = item.querySelector('img');
+
+            otherAnswer.classList.remove('active');
+            otherIcon.src = 'assets/images/icon-plus.svg';
+        })
         answer.classList.toggle('active');
 
         if(answer.classList.contains('active')) {
